4e6b6c1cfbad49e6b95e1aa585e31fac1da2f6bc
wicked9tailfox <wangfeicomeon@126.com>
2016-04-05 00:29:59 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,13 +1,7 @@
-resLoader.createCat('工具')
-.addItem('bejson','http://www.bejson.com/','在线工具,可视化,域名注册,在线json格式化工具,OCR图片转文字')
-.addItem('bejson','http://www.bejson.com/','在线工具,可视化,域名注册,在线json格式化工具,OCR图片转文字')
-.addItem('bejson','http://www.bejson.com/','在线工具,可视化,域名注册,在线json格式化工具,OCR图片转文字')
-.addItem('bejson','http://www.bejson.com/','在线工具,可视化,域名注册,在线json格式化工具,OCR图片转文字')
-;
 
-resLoader.createCat('工具')
-.addItem('bejson','http://www.bejson.com/','在线工具,可视化,域名注册,在线json格式化工具,OCR图片转文字')
-.addItem('bejson','http://www.bejson.com/','在线工具,可视化,域名注册,在线json格式化工具,OCR图片转文字');
+resLoader.createCat('CDN')
+.addItem('百度cdn','http://cdn.code.baidu.com/','')
+.addItem('cdnjs.com','https://cdnjs.com/','国外著名js cdn');
 
 resLoader.render();
 
