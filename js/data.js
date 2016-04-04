6b9d4b231115e9287951a2426dc9e975cbc53412
wicked9tailfox <wangfeicomeon@126.com>
2016-04-05 00:35:12 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,7 +1,12 @@
+resLoader.createCat('工具')
+.addItem('compressjpeg.com','http://compressjpeg.com/zh/','图片压缩')
+.addItem('tinypng.com','https://tinypng.com/','图片压缩,png压缩')
+;
 
 resLoader.createCat('CDN')
 .addItem('百度cdn','http://cdn.code.baidu.com/','')
-.addItem('cdnjs.com','https://cdnjs.com/','国外著名js cdn');
+.addItem('cdnjs.com','https://cdnjs.com/','国外著名js cdn')
+;
 
 resLoader.render();
 
