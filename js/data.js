01a7116200e0a1cf7ee0bae48a56c18d4ff07b56
wicked9tailfox <wangfeicomeon@126.com>
2016-04-06 10:59:39 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,7 @@
 resLoader.createCat('工具')
 .addItem('compressjpeg.com','http://compressjpeg.com/zh/','图片压缩')
 .addItem('tinypng.com','https://tinypng.com/','图片压缩,png压缩')
+.addItem('在线工具','http://tool.lu/','在线工具')
 ;
 
 resLoader.createCat('CDN')
@@ -8,6 +9,10 @@ resLoader.createCat('CDN')
 .addItem('cdnjs.com','https://cdnjs.com/','国外著名js cdn')
 ;
 
+resLoader.createCat('资料图书')
+.addItem('gitbook','https://www.gitbook.com','很多大牛存在，文章和总结')
+.addItem('Leo Hui的gitbook','https://www.gitbook.com/@leohxj','程序员的自我养成;前端工程师手册;学好node开发;Git学习与使用')
+;
 resLoader.render();
 
 
