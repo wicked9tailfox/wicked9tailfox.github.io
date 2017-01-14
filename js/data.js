f8fc4723f274c0d07172361c9de9c9ed63673d04
wicked9tailfox <wangfeicomeon@126.com>
2017-01-14 19:52:31 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -50,6 +50,7 @@ resLoader.createCat('资源')
 
 resLoader.createCat('文档')
 .addItem("sass官方文档",'http://sass-lang.com/documentation/file.SASS_REFERENCE.html#using_sass','sass官方文档')
+.addItem("whatwg标准","https://whatwg.org/","whatwg标准")
 .addItem("sass中文教程",'http://www.w3cplus.com/sassguide/syntax.html','简单，明了，实用');
 
 resLoader.createCat('浏览器')
